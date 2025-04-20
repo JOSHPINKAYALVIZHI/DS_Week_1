56b79d4cd08eeb60252800a3bd71cb552ce861fe
JOSHPINKAYALVIZHI <joshpinkayalvizhi1247@gmail.com>
2025-04-21 00:12:46 +0530

    updated calculator

--- Calculator.py
+++ Calculator.py
@@ -1,24 +1,47 @@
-num_1=int(input ("Enter number_1 :"))
-num_2=int(input ("Enter number_2 :"))
-print("1.[+]")
-print("2.[-]")
-print("3.[*]")
-print("4.[/]")
-operation=int(input("Enter the operation to done :"))
-def switch(operation):
-    if operation == 1:
-        print(num_1 ," + ",num_2 ," : " , num_1+num_2)
-    elif operation == 2:
-        print(num_1 ," - ",num_2 ," : " , num_1-num_2)
-    elif operation == 3:
-        print(num_1 ," * ",num_2 ," : " , num_1*num_2)
-    elif operation == 4:
-       if num_2 != 0:
-            print(num_1, "/", num_2, ":", num_1 / num_2)
-       else:
-            print("Division by zero is not allowed.")
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    if num_2 != 0:
+        return num_1 / num_2
     else:
-        print("Invalid operation..!!")
+        return "Division by zero is not allowed."
+
+def calculator():
+    try:
+        num_1 = int(input("Enter number 1: "))
+        num_2 = int(input("Enter number 2: "))
         
-switch(operation)
+        print("Select operation:")
+        print("1. [+] Addition")
+        print("2. [-] Subtraction")
+        print("3. [*] Multiplication")
+        print("4. [/] Division")
+        
+        operation = int(input("Enter the operation number (1/2/3/4): "))
+        
+        if operation == 1:
+            print(f"{num_1} + {num_2} = {add(num_1, num_2)}")
+        elif operation == 2:
+            print(f"{num_1} - {num_2} = {subtract(num_1, num_2)}")
+        elif operation == 3:
+            print(f"{num_1} * {num_2} = {multiply(num_1, num_2)}")
+        elif operation == 4:
+            print(f"{num_1} / {num_2} = {divide(num_1, num_2)}")
+        else:
+            print("Invalid operation. Please select 1, 2, 3, or 4.")
+    
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
+    
+    finally:
+        print("\nCalculator operation has finished.")  
+calculator()
+
 
